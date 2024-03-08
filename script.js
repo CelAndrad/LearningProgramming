3f67d147399a328477ff054ff2b14e614dd6eafa
CelAndrad <cel.andrad@gmail.com>
2024-03-07 21:50:00 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/assets/fotoClara.png")
+    img.setAttribute("alt", "Um texto qualquer pro light mode")
+  } else {
+    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("alt", "Um texto qualquer pro Dark mode")
+  }
+}
